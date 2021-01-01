3c7210c365579a39649f27d1016029f032089e5d
ghanad <48297368+ghanad@users.noreply.github.com>
2021-01-02 00:13:30 +0330

    use FileReadWriteHandler

--- src/com/ghandi/Main.java
+++ src/com/ghandi/Main.java
@@ -1,11 +1,15 @@
 package com.ghandi;
 
 
-import java.util.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
-        String tim = new String();
+    public static void main(String[] args) throws IOException {
+        String src_path = "C:\\Users\\ali\\Desktop\\java_test2.txt";
+        FileReadWriteHandle tt = new FileReadWriteHandle(src_path);
+
     }
 }
